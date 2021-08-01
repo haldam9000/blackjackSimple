175e136b05c10b55222daa70e0570ba98980e324
haldam9000 <85191578+haldam9000@users.noreply.github.com>
2021-08-01 14:03:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function startGame() {
     sum = firstCard + secondCard
     renderGame()
 }
-//writen by haldam9000
+
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
@@ -38,12 +38,12 @@ function renderGame() {
     
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Want a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game!"
+        message = "You lose, try again!"
         isAlive = false
     }
     messageEl.textContent = message
